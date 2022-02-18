67e1770896f8aea51ddf6003dff633b41264cafe
Silvia Martinez <sil@silvia.com>
2022-02-18 20:44:53 +0100

    Update src/index.js with connect DB)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const debug = require("debug")("robots:root");
 const chalk = require("chalk");
-
+const connectDataBase = require("./database/index");
 const upServer = require("./server/index");
 
 const port = process.env.SERVER_PORT || 4000;
+const dbstring = process.env.MONGO_STRING;
 
 (async () => {
   try {
+    await connectDataBase(dbstring);
     await upServer(port);
   } catch (error) {
     debug(chalk.red(`Error: `, error.message));
