4c84413d6ef13c574d8672b702785433e73e18f7
Silvia Martinez <sil@silvia.com>
2022-02-18 20:20:28 +0100

    Update index.js in db

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+const debug = require("debug")("robots:database");
+const chalk = require("chalk");
+const mongoose = require("mongoose");
+
+const connectDataBase = (connectionString) =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(connectionString, (error) => {
+      if (error) {
+        reject(new Error(`Couldn't connect to the database: ${error.message}`));
+        return;
+      }
+      debug(chalk.bgMagenta("Connected to DB"));
+      resolve();
+    });
+  });
+
+module.exports = connectDataBase;
