e6d24095bb90f40dc97524cce044ae958b16ddca
Silvia Martinez <sil@silvia.com>
2022-02-19 11:10:45 +0100

    Rename SERVER_PORT to PORT and update debug in index/server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const chalk = require("chalk");
 const connectDataBase = require("./database/index");
 const upServer = require("./server/index");
 
-const port = process.env.SERVER_PORT || 4000;
+const port = process.env.PORT || 4000;
 const dbstring = process.env.MONGO_STRING;
 
 (async () => {
