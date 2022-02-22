4a8a3f4874a16e1a5f15cf91ed73d92717d6eb4d
Silvia Martinez <sil@silvia.com>
2022-02-22 17:47:36 +0100

    Extract upServer to put in upServer.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,4 @@
 require("dotenv").config();
-const debug = require("debug")("robots:server");
-const chalk = require("chalk");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -11,27 +9,11 @@ const { errorNotFound, errorDefault } = require("./middlewares/errors");
 
 const app = express();
 
-const upServer = (port) =>
-  new Promise((resolve, reject) => {
-    const server = app.listen(port, () => {
-      debug(
-        chalk.yellow(`Server listening on http://localhost:${port}/robots`)
-      );
-      resolve();
-    });
-
-    server.on("error", (error) => {
-      reject(error);
-    });
-  });
-
 app.use(morgan("dev"));
 
 app.use(helmet());
 
 app.use(cors());
-
-/* app.use(cors({ methods: ["GET", "PUT", "POST", "DELETE"] })); */
 
 app.use(express.json());
 
@@ -43,4 +25,4 @@ app.use(errorNotFound);
 
 app.use(errorDefault);
 
-module.exports = upServer;
+module.exports = { app };
